775bfee3e696276e53dc5b7349d0649ca426e442
du than <du.than@relexsolutions.com>
2025-03-11 09:42:39 +0700

    moved stdlib.h file

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 11:32:53 by dthan             #+#    #+#             */
-/*   Updated: 2024/04/21 00:27:52 by dthan            ###   ########.fr       */
+/*   Updated: 2025/03/11 09:42:19 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include "../list/ft_list.h"
 # include "../print/ft_print.h"
 # include "ft_stdio.h"
-# include "ft_stdlib.h"
+# include "../stdlib/ft_stdlib.h"
 # include "../string/ft_string.h"
 # include "ft_utility.h"
 # include "get_next_line.h"
